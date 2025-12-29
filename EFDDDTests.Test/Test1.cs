020a881e913850050d1b14245d1770764c1a0ca7
橋谷崇 <t61342006@yahoo.co.jp>
2025-12-29 18:48:14 +0900

    テストコード作成

--- EFDDDTests.Test/Test1.cs
+++ EFDDDTests.Test/Test1.cs
@@ -11,18 +11,19 @@ namespace EFDDDTests.Test
         [TestMethod]
         public void TestMethod1()
         {
-            var builder = new SqlConnectionStringBuilder();
-            builder.DataSource = @"(localdb)\MSSQLLocalDB";
-            builder.InitialCatalog = "AndersonD";
-            builder.IntegratedSecurity = true;
             var o = new DbContextOptionsBuilder<AndersonDBContext>();
-            o.UseSqlServer(builder.ConnectionString);
-
-
-            var context = new AndersonDBContext();
+            o.UseInMemoryDatabase("MyDataBase");
+            o.EnableServiceProviderCaching(false); // キャッシュの無効化
+            var context = new AndersonDBContext(o.Options);
             var unitOfWork = new UnitOfWork(context);
+
+            context.Products.Add(new EFDDD.Domain.Entities.ProductEntity(1, "A1", 111));
+            context.Products.Add(new EFDDD.Domain.Entities.ProductEntity(2, "A2", 222));
+            context.SaveChanges();
+
             var vm = new Form1ViewModel(unitOfWork);
 
+            Assert.AreEqual(2, vm.Products.Count());
         }
     }
 }
